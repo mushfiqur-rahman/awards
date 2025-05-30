bf367c5fcdb0b78fd1b8fd61bb1e450597ae1bab
mushfiqur-rahman <mushfiq.style@gmail.com>
2025-05-30 08:02:09 +0600

    initial

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const App = () => {
+    return (
+        <><h2 className="text-5xl font-bold text-orange-500">Welcome To awards</h2></>
+    )
+}
+export default App
---
+++ vite.config.js
@@ -0,0 +1,34 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  theme: {
+    extend: {
+      fontFamily: {
+        zentry: ["zentry", "sans-serif"],
+        general: ["general", "sans-serif"],
+        "circular-web": ["circular-web", "sans-serif"],
+        "robert-medium": ["robert-medium", "sans-serif"],
+        "robert-regular": ["robert-regular", "sans-serif"],
+      },
+      colors:{
+        blue: {
+          50: "#DFDFF0",
+          75: "#dfdff2",
+          100: "#F0F2FA",
+          200: "#010101",
+          300: "#4FB7DD",}
+      },
+      violet: {
+        300: "#5724ff",
+      },
+      yellow: {
+        100: "#8e983f",
+        300: "#edff66",
+      },
+    }
+  },
+  plugins: [react(), tailwindcss()],
+})
